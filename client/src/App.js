47994eb6c38df1156b870caf23e57423173e7266
Fidal <fidal15perfect@gmail.com>
2022-05-20 15:19:58 +0530

    UI improve

--- client/src/App.js
+++ client/src/App.js
@@ -234,8 +234,9 @@ const App = () => {
 	// Render methods
 	const renderNotConnectedContainer = () => (
 		<div className="connect-wallet-container">
-			<img src="https://media.giphy.com/media/3ohhwytHcusSCXXOUg/giphy.gif" alt="Ninja donut gif" />
+			<img src="https://c.tenor.com/ROs-KMJ8vD8AAAAC/deku-mha.gif" alt="midoriya gif" />
 			{/* Call the connectWallet function we just wrote when the button is clicked */}
+
 			<button onClick={connectWallet} className="cta-button connect-wallet-button">
 				Connect Wallet
 			</button>
@@ -270,7 +271,7 @@ const App = () => {
 				<input
 					type="text"
 					value={record}
-					placeholder='whats ur ninja power?'
+					placeholder='whats ur  power?'
 					onChange={e => setRecord(e.target.value)}
 				/>
 				{/* If the editing variable is true, return the "Set record" and "Cancel" button */}
@@ -340,8 +341,8 @@ const App = () => {
 				<div className="header-container">
 					<header>
 						<div className="left">
-							<p className="title">🐱‍👤 Ninja Name Service</p>
-							<p className="subtitle">Your immortal API on the blockchain!</p>
+							<p className="title">NFT Name Service</p>
+							<p className="subtitle">Create endless NFTs on the blockchain!</p>
 						</div>
 						{/* Display a logo and wallet connection status*/}
 						<div className="right">
